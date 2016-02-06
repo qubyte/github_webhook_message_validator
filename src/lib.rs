aac3b1a1a681e5cc5ded1cbbf52851854636dea8
Mark S. Everitt <mark.s.everitt@gmail.com>
2016-02-06 22:05:42 +0000

    Adds code, docs, and tests.

---
+++ src/lib.rs
@@ -0,0 +1,69 @@
+//! The `github_webhook_message_validator` crate provides functions to validating GitHub webhook
+//! notifications.
+
+extern crate crypto;
+
+use crypto::hmac::Hmac;
+use crypto::mac::{Mac, MacResult};
+use crypto::sha1::Sha1;
+
+
+/// Check a signature and a message against a shared secret.
+///
+/// Note that if you get the signature from the `X-Hub-Signature` header, you'll need to convert it
+/// to bytes via hex. Use the `rustc_serialize` `From_Hex` trait to do this.
+///
+/// # Examples
+///
+/// ```
+/// use github_webhook_message_validator::validate;
+///
+/// let signature = &vec![
+///     0x73, 0x6d, 0x7f, 0x93, 0x42,
+///     0xf2, 0xa7, 0xd2, 0x39, 0xaf,
+///     0xa5, 0x51, 0x3a, 0x4b, 0xb2,
+///     0x28, 0x3e, 0x0e, 0x15, 0x88
+/// ];
+/// let secret = "some-secret".as_bytes();
+/// let message = "blah-blah-blah".as_bytes();
+///
+/// assert_eq!(validate(secret, signature, message), true);
+/// ```
+pub fn validate(secret: &[u8], signature: &[u8], message: &[u8]) -> bool {
+    let mut hmac = Hmac::new(Sha1::new(), secret);
+    hmac.input(&message[..]);
+    hmac.result() == MacResult::new(&signature[..])
+}
+
+#[cfg(test)]
+mod test {
+    use validate;
+
+    #[test]
+    fn it_returns_true_when_signature_and_message_match() {
+        let signature = &vec![
+            0x73, 0x6d, 0x7f, 0x93, 0x42,
+            0xf2, 0xa7, 0xd2, 0x39, 0xaf,
+            0xa5, 0x51, 0x3a, 0x4b, 0xb2,
+            0x28, 0x3e, 0x0e, 0x15, 0x88
+        ];
+        let secret = "some-secret".as_bytes();
+        let message = "blah-blah-blah".as_bytes();
+
+        assert_eq!(validate(secret, signature, message), true);
+    }
+
+    #[test]
+    fn it_returns_false_when_signature_and_message_do_not_match() {
+        let signature = &vec![
+            0x31, 0x30, 0x2b, 0x00, 0xba,
+            0xd4, 0xd6, 0xd1, 0x10, 0xa1,
+            0x18, 0x82, 0x77, 0xc4, 0xd1,
+            0x06, 0x0c, 0xb2, 0xc3, 0x73
+        ];
+        let secret = "some-secret".as_bytes();
+        let message = "blah-blah-blah?".as_bytes();
+
+        assert_eq!(validate(secret, signature, message), false);
+    }
+}

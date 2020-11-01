8a47833a80ad90db4bcd2694ec7e10f9226f186d
Konrad Borowski <konrad@borowski.pw>
2020-11-01 15:08:01 +0100

    Update dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 extern crate hmac;
 extern crate sha1;
 
-use hmac::{Hmac, Mac};
+use hmac::{Hmac, Mac, NewMac};
 use sha1::Sha1;
 
 
@@ -27,8 +27,9 @@ use sha1::Sha1;
 /// assert_eq!(validate(secret, signature, message), true);
 /// ```
 pub fn validate(secret: &[u8], signature: &[u8], message: &[u8]) -> bool {
-    let mut hmac = Hmac::<Sha1>::new_varkey(secret).unwrap();
-    hmac.input(message);
+    let mut hmac = Hmac::<Sha1>::new_varkey(secret)
+        .expect("HMAC can take key of any size");
+    hmac.update(message);
     hmac.verify(signature).is_ok()
 }
 
